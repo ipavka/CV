7745eea842c40930dac31c97a488b13aa1b9e0e4
ipavka <ipavka@ya.ru>
2022-05-16 11:04:22 +0300

    v_1.0.6 test backgroundImage

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -4,19 +4,23 @@ import sContainer from '../../common/styles/Container.module.scss';
 import sItem from '../../common/styles/Item.module.css';
 import {Project} from "./project/Project";
 import {TitleCustom} from "../../common/com-components/TitleCustom";
+import todoImage from '../../assets/image/todo_list.png'
+import socialNet from '../../assets/image/social_network.png'
 
 const projects = [
     {
         id: 1,
-        url: '#',
-        header: 'My Facebook',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        url: todoImage,
+        header: 'ToDo List',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        todoList: {backgroundImage: `url(${todoImage})`}
     },
     {
         id: 2,
-        url: '#',
-        header: 'My Twitter',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        url: socialNet,
+        header: 'Social Network',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+        todoList: {backgroundImage: `url(${socialNet})`}
     },
 
 ]
@@ -31,6 +35,7 @@ export const Projects = () => {
                     return <Project key={el.id}
                                     url={el.url}
                                     header={el.header}
+                                    img={el.todoList}
                                     description={el.description}/>
                 })}
             </div>
--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -4,14 +4,15 @@ import s from './Project.module.scss';
 type ProjectPropsType = {
     id?: number
     url: string
+    img: {backgroundImage: string}
     header: string
     description: string
 }
 export const Project: React.FC<ProjectPropsType> = props => {
     return (
         <div className={s.projectBlock}>
-            <div className={s.urlProjectContent}>
-                <a href={props.url}>Watch</a>
+            <div className={s.urlProjectContent} style={props.img} >
+                <a className={s.viewBtn} href={props.url} target="_blank">Watch</a>
             </div>
             <h2>{props.header}</h2>
             <span>{props.description}</span>
