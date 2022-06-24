d510e0b3418a42e2e9ad1d6e16ea2bd78460971b
ipavka <ipavka@ya.ru>
2022-06-24 14:07:50 +0300

    new version

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,8 +7,8 @@ import telegramImg from '../../assets/image/telegram.png'
 import email from '../../assets/image/email_1.png'
 import gitHub from '../../assets/image/github_2.png'
 import codewars from '../../assets/image/codewars_yellow.png'
-
 const Fade = require("react-reveal/Fade");
+
 
 export const Footer = () => {
   return (
@@ -17,10 +17,14 @@ export const Footer = () => {
         <div className={`${sItem.item} ${s.footerItems}`}>
           <TitleCustom title={'Pavel'} hLevel={2} costumeStyle={s.title}/>
           <div className={s.bioLink}>
-            <a href="#" style={{backgroundImage: `url(${telegramImg})`}}/>
-            <a href="#" style={{backgroundImage: `url(${email})`}}/>
-            <a href="#" style={{backgroundImage: `url(${gitHub})`}}/>
-            <a href="#" style={{backgroundImage: `url(${codewars})`}}/>
+            <a href="https://t.me/ipavka" target="_blank"
+               style={{backgroundImage: `url(${telegramImg})`}}/>
+            <a href="https://mail.google.com" target="_blank"
+               style={{backgroundImage: `url(${email})`}}/>
+            <a href="https://github.com/i-pavka" target="_blank"
+               style={{backgroundImage: `url(${gitHub})`}}/>
+            <a href="https://www.codewars.com/users/ipavka" target="_blank"
+               style={{backgroundImage: `url(${codewars})`}}/>
           </div>
         </div>
       </Fade>
