14c7a4a558ba9fef346693695c50952dcc5b3ccf
ipavka <72130001+ipavka@users.noreply.github.com>
2022-06-30 12:59:56 +0300

    Update api.ts

--- src/store/api.ts
+++ src/store/api.ts
@@ -6,8 +6,8 @@ const {FORM_ENDPOINT_DEV, FORM_ENDPOINT} = apiConfig;
 
 export const formsAPI = {
   sendMessage(data: FormType) {
-    // return axios.post(`https://formspree.io/f/${FORM_ENDPOINT}`, data)
-    return axios.post(`https://formspree.io/f/${FORM_ENDPOINT_DEV}`, data)
+    return axios.post(`https://formspree.io/f/${FORM_ENDPOINT}`, data)
+   // return axios.post(`https://formspree.io/f/${FORM_ENDPOINT_DEV}`, data)
       .then(res => {
         return res.data
       })
